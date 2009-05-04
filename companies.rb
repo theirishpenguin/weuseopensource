1ef4d4d2de4be02ec0d135e392abb7c80b3ca3c4
theirishpenguin <declanmg@gmail.com>
2009-05-04 17:47:28 +0100

    Added a basic HTML SELECT helper.

--- companies.rb
+++ companies.rb
@@ -192,6 +192,22 @@ helpers do
     msg
   end
 
+  # Basic implementation of a HTML SELECT helper
+  def select(resource_name, field_name, options_list, selected_value)
+    html = ''
+    options_list.each do |nv_pair|
+      option_value = nv_pair.keys.first
+      option_name = nv_pair.values.first
+      html << "<option value=\"#{option_value}\""
+      html << " selected=\"true\"" if option_value == selected_value
+      html << '>'
+      html << option_name
+      html << "</option>"
+    end
+    "<select name=\"#{resource_name}_#{field_name}\">#{html}</select>"
+  end
+
+
 end
 
 ### ### ### ### ### ###
